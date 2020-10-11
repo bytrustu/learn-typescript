add987f8d8f7db82251068e5f9c2df9a4cbcba8d
bytrustu <bytrustu@gmail.com>
2020-10-12 01:31:43 +0900

    function add todo

--- src/todo_list/src/Command.ts
+++ src/todo_list/src/Command.ts
@@ -1,5 +1,7 @@
-import {AppState} from "./type";
+import {Action, ActionNewTodo, AppState, Priority, PRIORITY_NAME_MAP} from "./type";
 import {waitForInput} from "./Input";
+import {constants} from "os";
+import {getIsValidEnumValue} from "./util";
 
 export abstract class Command {
     constructor(public key: string, private desc: string) {
@@ -9,7 +11,7 @@ export abstract class Command {
         return `${this.key}: ${this.desc}`;
     }
 
-    abstract async run(state: AppState): Promise<void>;
+    abstract async run(state: AppState): Promise<void | Action>;
 }
 
 export class CommandPrintTodos extends Command {
@@ -22,5 +24,26 @@ export class CommandPrintTodos extends Command {
             console.log(text);
         }
         await waitForInput('press any key : ');
+    }
+}
+
+export class CommandNewTodo extends Command {
+    constructor() {
+        super('n', '할 일 추가하기');
+    }
+    async run(): Promise<void | ActionNewTodo> {
+        const title = await waitForInput('제 : ');
+        const priorityStr = await waitForInput(`우선순위 ${PRIORITY_NAME_MAP[Priority.High]}(${Priority.High}) ~ ${PRIORITY_NAME_MAP[Priority.Low]}(${Priority.Low}): `);
+        const priority = Number(priorityStr);
+        if (title && CommandNewTodo.getIsPriority(priority)) {
+            return {
+                type: 'newTodo',
+                title,
+                priority
+            }
+        }
+    }
+    static getIsPriority(priority: number): priority is Priority {
+        return getIsValidEnumValue(Priority, priority);
     }
 }
